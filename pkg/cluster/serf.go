3d257be716c2e6e5107421a0e21f038e07d4fd56
vistara-dev <dev@vistara.dev>
2025-06-26 20:56:44 +0530

    fix: bump cpu

--- pkg/cluster/serf.go
+++ pkg/cluster/serf.go
@@ -158,7 +158,7 @@ func (a *Agent) handleSpawnRequest(payload *pb.VmSpawnRequest) (ret []byte, retE
 		memUsed += int(labelPayload.GetMemory())
 	}
 
-	if (vcpuUsed + int(payload.GetCores())) > max(runtime.NumCPU(), 100) {
+	if (vcpuUsed + int(payload.GetCores())) > max(runtime.NumCPU(), 300) {
 		return nil, fmt.Errorf("cannot spawn container: have capacity for %d vCPUs, already in use: %d, requested: %d", runtime.NumCPU(), vcpuUsed, payload.GetCores())
 	}
 
